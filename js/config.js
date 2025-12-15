68a4f9b303c2999572db216d2c4629e52f8543c7
xpycy7hfrq <50142257-xpycy7hfrq@users.noreply.replit.com>
2025-12-15 18:09:18 +0000

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window.ENV = window.ENV || {};
+window.ENV.mode = 'WEB';
+window.ENV.validateBrowser = true;
+window.ENV.production = 'production';
+
+window.ENV.tcApp = 'https://tcdigital.bancodebogota.com/web-card/info-motor/';
+window.ENV.rnec = 'https://authenticatorweb.bancodebogota.com.co/';
+window.ENV.apiEndpointEvents = 'https://api.bancodebogota.co/event/';
+window.ENV.rnecEndpoint = 'https://api.bancodebogota.co/auth/';
+window.ENV.container = 'GTM-N5DKC46';
